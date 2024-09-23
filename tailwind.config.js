c872097013d623522687950f88bab65410b7001a
Nguyễn Tiến Đạt <tymmin146@gmail.com>
2024-09-23 21:20:46 +0700

    quanao

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        screens: {
+          'sm': '576px',
+          'md': '768px',        
+          'lg': '992px',     
+          'xl': '1240px',      
+          '2xl':'1240px',
+        },
+        extend: {
+        colors: {
+           main:'#000000',
+           light:'#00000099',
+        }
+        }   
+    }
+}
